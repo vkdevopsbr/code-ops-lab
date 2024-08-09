0774b4b6602c1d293176d158e57dcf529f2fef66
Victor Hugo Lima <147187329+vkdevopsbr@users.noreply.github.com>
2024-08-09 22:54:53 +0000

    new scipts  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # code-ops-lab
+
 Meu laboratório de aprendizado para explorar programação e práticas DevOps, focado em me tornar um profissional mais completo, unindo código e operações.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h1>Estou estudando java script e novas tecnologias pensando no meu filho com a minha amada Laiza.</h1>
+  return (
+    <h1>
+      Estou estudando java script e novas tecnologias pensando no meu filho com
+      a minha amada Laiza.
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
-
