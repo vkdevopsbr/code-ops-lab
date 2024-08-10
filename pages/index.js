45fcc32affde172454de9854ec0530e895aba578
Victor Hugo Lima <147187329+vkdevopsbr@users.noreply.github.com>
2024-08-10 09:06:08 +0000

    install jest and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 function Home() {
   return (
     <h1>
-      Estou estudando java script e novas tecnologias pensando no meu filho com
-      a minha amada Laiza.
+      Estou estudando JavaScript e novas tecnologias pensando no meu filho, Gael
+      Jesus. A mulher que amo, Laiza Moura, carrega nosso precioso em seu
+      ventre.
     </h1>
   );
-}
-
-function teste2() {
-  console.log("indentação errada");
 }
 
 export default Home;
